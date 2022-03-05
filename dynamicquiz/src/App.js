772905a97f3f4022a010d09c70b711999bce0690
nithuntr <nithuntr19@gmail.com>
2022-03-05 15:45:39 +0530

    quiz 2

---
+++ dynamicquiz/src/App.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import HomeComponent from './components/HomeComponent';
+import ResultComponent from './components/ResultComponent';
+import QuizComponent from './components/QuizComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    {/*<HomeComponent/>
+    <ResultComponent/>*/}
+    <QuizComponent/>
+    </div>
+  );
+}
+
+export default App;
